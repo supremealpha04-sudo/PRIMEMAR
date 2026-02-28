c1fee2a2a4c961644e1e8ec9358fa2b0ed4ebde0
supremealpha04-sudo <supremealpha04@gmail.com>
2026-02-28 11:31:03 -0800

    Create security_service.dart

---
+++ lib/core/providers/security_service.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import '../services/auth_service.dart';
+
+final authServiceProvider = Provider<AuthService>((ref) => AuthService());
+
+final authStateProvider = StreamProvider<AuthState>((ref) {
+  return ref.watch(authServiceProvider).authStateChanges;
+});
+
+final currentUserProvider = Provider<User?>((ref) {
+  return ref.watch(authServiceProvider).currentUser;
+});
+
+final isAuthenticatedProvider = Provider<bool>((ref) {
+  return ref.watch(currentUserProvider) != null;
+});
